e5ef6e095e98fd097481437a2da7ccada2d8900c
JohanCederschiold <johan.cederschiold@gmail.com>
2020-01-23 21:26:22 +0100

    More work on cors

--- src/main/java/se/jpdc/receptdemo/ReceptdemoApplication.java
+++ src/main/java/se/jpdc/receptdemo/ReceptdemoApplication.java
@@ -2,6 +2,8 @@ package se.jpdc.receptdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ReceptdemoApplication {
@@ -11,3 +13,4 @@ public class ReceptdemoApplication {
     }
 
 }
+
